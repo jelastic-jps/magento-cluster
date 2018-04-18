a35c41b320644218f194ced92030e71daedec2fc
sych74 <sych74@gmail.com>
2018-04-18 13:03:43 +0300

    create template for config

--- configs/magento/env.php
+++ configs/magento/env.php
@@ -6,46 +6,64 @@ return array (
   ),
   'crypt' =>
   array (
-    'key' => 'de7fc043d94949818bbe40cba5b2fde4',
+    'key' => 'f570d743c4f353262b241ef4122f13bb',
   ),
 
 'session' =>
-   array (
-      'save' => 'memcached',
-      'save_path' => 'MEMCACHE:11211'
+array (
+  'save' => 'redis',
+  'redis' =>
+  array (
+    'host' => 'RDS',
+    'port' => '6379',
+    'password' => '_RDPASS_',
+    'timeout' => '2.5',
+    'persistent_identifier' => '',
+    'database' => '2',
+    'compression_threshold' => '2048',
+    'compression_library' => 'gzip',
+    'log_level' => '3',
+    'max_concurrency' => '6',
+    'break_after_frontend' => '5',
+    'break_after_adminhtml' => '30',
+    'first_lifetime' => '600',
+    'bot_first_lifetime' => '60',
+    'bot_lifetime' => '7200',
+    'disable_locking' => '0',
+    'min_lifetime' => '60',
+    'max_lifetime' => '2592000'
+  )
 ),
-
-
-
-    'cache' =>
-        array (
-            'frontend' => array(
-                'default' => array(
-                    'backend' => 'Cm_Cache_Backend_Redis',
-                    'backend_options' => array(
-                        'server' => 'REDIS',            // or absolute path to unix socket
-                        'port' => '6379',
-                        'persistent' => '',                 // Specify a unique string like "cache-db0" to enable persistent connections.
-                        'database' => '0',
-                        'password' => '',
-                        'force_standalone' => '0',          // 0 for phpredis, 1 for standalone PHP
-                        'connect_retries' => '1',           // Reduces errors due to random connection failures
-                        'read_timeout' => '10',             // Set read timeout duration
-                        'automatic_cleaning_factor' => '0', // Disabled by default
-                        'compress_data' => '1',             // 0-9 for compression level, recommended: 0 or 1
-                        'compress_tags' => '1',             // 0-9 for compression level, recommended: 0 or 1
-                        'compress_threshold' => '20480',    // Strings below this size will not be compressed
-                        'compression_lib' => 'gzip',        // Supports gzip, lzf and snappy,
-                        'use_lua' => '0'                    // Lua scripts should be used for some operations
-                    )
-                )
-            )
-        ),
-
-
-
-
-  'db' =>
+'cache' =>
+array(
+   'frontend' =>
+   array(
+      'default' =>
+      array(
+         'backend' => 'Cm_Cache_Backend_Redis',
+         'backend_options' =>
+         array(
+            'server' => 'RDC',
+            'password' => '_RDPASS_',
+            'database' => '0',
+            'port' => '6379'
+            ),
+    ),
+    'page_cache' =>
+    array(
+      'backend' => 'Cm_Cache_Backend_Redis',
+      'backend_options' =>
+       array(
+         'server' => 'RDC',
+         'port' => '6379',
+         'password' => '_RDPASS_',
+         'database' => '1',
+         'compress_data' => '0'
+       )
+    )
+  )
+),
+ 'db' =>
   array (
     'table_prefix' => '',
     'connection' =>
@@ -62,23 +80,18 @@ return array (
         'active' => '1',
       ),
     ),
-
-
-        'slave_connection' =>
-            array (
-                'default' =>
-                    array (
-                        // HaProxy Read (slave) connection
-                        'host' => 'DB_SLAVE',
-                        'dbname' => '_DBNAME_',
-                        'username' => '_DBUSER_',
-                        'password' => '_DBPASS_',
-                        'active' => '1',
-                    ),
-            ),
-
-
-
+   'slave_connection' =>
+    array (
+      'default' =>
+      array (
+        'host' => 'DB_SLAVE',
+        'dbname' => '_DBNAME_',
+        'username' => '_DBUSER_',
+        'password' => '_DBPASS_',
+        'active' => '1',
+      ),
+    ),
+    'table_prefix' => '',
   ),
   'resource' =>
   array (
@@ -107,6 +120,6 @@ return array (
   ),
   'install' =>
   array (
-    'date' => 'Fri, 15 Sep 2017 14:41:38 +0000',
+    'date' => 'Wed, 18 Apr 2018 08:11:54 +0000',
   ),
 );

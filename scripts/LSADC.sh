5636f9be7f772045b01ff04baa70a3f60e7247e8
HENADII SYCHEVSKYI <SGI@HENADIIs-MacBook-Pro-4.local>
2020-02-27 11:40:38 +0200

    Added optimization for LB

--- scripts/LSADC.sh
+++ scripts/LSADC.sh
@@ -4,20 +4,7 @@ LSLB_CONF="/var/www/conf/lslbd_config.xml"
 VH_CONF="/var/www/conf/jelastic.xml"
 ED_CMD="ed --inplace"
 
-WP_PROTECT_ACTION=3
-WP_PROTECT_LIMIT=100
-
 cp -f ${LSLB_CONF} ${LSLB_CONF}.backup.$(date +%d-%m-%Y.%H:%M:%S.%N) || exit 1
-
-#Wordpress Brute Force Attacks Protect
-/usr/bin/xmlstarlet ${ED_CMD} -d "loadBalancerConfig/security/wpProtectAction" "${LSLB_CONF}" 2>/dev/null;
-/usr/bin/xmlstarlet ${ED_CMD} -d "loadBalancerConfig/security/wpProtectLimit" "${LSLB_CONF}" 2>/dev/null;
-/usr/bin/xmlstarlet ${ED_CMD} -d "virtualHostConfig/security/wpProtectAction" "${VH_CONF}" 2>/dev/null;
-/usr/bin/xmlstarlet ${ED_CMD} -d "virtualHostConfig/security/wpProtectLimit" "${VH_CONF}" 2>/dev/null;
-/usr/bin/xmlstarlet ${ED_CMD} -s "loadBalancerConfig/security" -t elem -n "wpProtectLimit" -v "${WP_PROTECT_LIMIT}" "${LSLB_CONF}" 2>/dev/null;
-/usr/bin/xmlstarlet ${ED_CMD} -s "loadBalancerConfig/security" -t elem -n "wpProtectAction" -v "${WP_PROTECT_ACTION}" "${LSLB_CONF}" 2>/dev/null;
-/usr/bin/xmlstarlet ${ED_CMD} -s "virtualHostConfig/security" -t elem -n "wpProtectLimit" -v "${WP_PROTECT_LIMIT}" "${VH_CONF}" 2>/dev/null;
-/usr/bin/xmlstarlet ${ED_CMD} -s "virtualHostConfig/security" -t elem -n "wpProtectAction" -v "${WP_PROTECT_ACTION}" "${VH_CONF}" 2>/dev/null;
 
 #Load Balancer Optimization
 /usr/bin/xmlstarlet ${ED_CMD} -u "loadBalancerConfig/loadBalancerList/loadBalancer[name = "*"]/workerGroupList/workerGroup[name = "*"]/maxConns" -v "150" "${LSLB_CONF}" 2>/dev/null;
@@ -25,4 +12,14 @@ cp -f ${LSLB_CONF} ${LSLB_CONF}.backup.$(date +%d-%m-%Y.%H:%M:%S.%N) || exit 1
 /usr/bin/xmlstarlet ${ED_CMD} -u "loadBalancerConfig/loadBalancerList/loadBalancer[name = "*"]/workerGroupList/workerGroup/pingInterval" -v "1" "${LSLB_CONF}" 2>/dev/null;
 /usr/bin/xmlstarlet ${ED_CMD} -u "loadBalancerConfig/loadBalancerList/loadBalancer[name = "*"]/strategy" -v "3" "${LSLB_CONF}" 2>/dev/null;
 
+#Enable LiteMage
+/usr/bin/xmlstarlet ${ED_CMD} -u "virtualHostConfig/cache/storage/litemage" -v "1" "${VH_CONF}" 2>/dev/null;
+
+#Enable  cachePolicy
+/usr/bin/xmlstarlet ${ED_CMD} -d "virtualHostConfig/cache/cachePolicy" "${VH_CONF}" 2>/dev/null;
+/usr/bin/xmlstarlet ${ED_CMD} -s "virtualHostConfig/cache/cachePolicy"  -t elem -n "checkPublicCache" -v "1" "${VH_CONF}" 2>/dev/null;
+/usr/bin/xmlstarlet ${ED_CMD} -s "virtualHostConfig/cache/cachePolicy"  -t elem -n "checkPrivateCache" -v "1" "${VH_CONF}" 2>/dev/null;
+/usr/bin/xmlstarlet ${ED_CMD} -s "virtualHostConfig/cache/cachePolicy"  -t elem -n "cacheStaticFile" -v "15" "${VH_CONF}" 2>/dev/null;
+
+# Reload service
 sudo service lslb reload

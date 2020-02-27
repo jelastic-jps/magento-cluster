0eeecc0a091a316e592003496f4569ffac5e1edd
HENADII SYCHEVSKYI <SGI@HENADIIs-MacBook-Pro-4.local>
2020-02-27 17:03:00 +0200

    Changed version for balancer

--- scripts/beforeinstall.js
+++ scripts/beforeinstall.js
@@ -91,7 +91,7 @@ if (${settings.ls-addon:false}) {
 if (!${settings.ls-addon:false}) {
   resp.nodes.push({
     nodeType: "varnish",
-    tag: "6.3.2",
+    tag: "5.2.1",
     count: 1,
     flexibleCloudlets: ${settings.bl_flexibleCloudlets:8},
     fixedCloudlets: ${settings.bl_fixedCloudlets:1},

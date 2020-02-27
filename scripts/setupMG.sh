ac9b432df05f1ba654858b113a7386e8c0ed13b3
HENADII SYCHEVSKYI <SGI@HENADIIs-MacBook-Pro-4.local>
2020-02-27 14:48:39 +0200

    Fixed setup script

--- scripts/setupMG.sh
+++ scripts/setupMG.sh
@@ -133,7 +133,7 @@ if [ $litemage == 'true' ] ; then
   SHORT_VERSION=$(echo ${VERSION} | sed 's/v//')
   $WGET https://github.com/${GITHUB_LITEMAGE_SOURCE}/archive/${VERSION}.tar.gz -O /tmp/${VERSION}.tgz
   $TAR -C "/tmp" -xpzf "/tmp/${VERSION}.tgz";
-  [ -d ${SERVER_WEBROOT}/app/code/Litespeed/Litemage ] && mkdir -p ${SERVER_WEBROOT}/app/code/Litespeed/Litemage
+  [ -d ${SERVER_WEBROOT}/app/code/Litespeed/Litemage ] || mkdir -p ${SERVER_WEBROOT}/app/code/Litespeed/Litemage
   $RSYNC -au --remove-source-files /tmp/magento2-LiteSpeed_LiteMage-${SHORT_VERSION}/ ${SERVER_WEBROOT}/app/code/Litespeed/Litemage/
   ${MG} module:enable Litespeed_Litemage &>> /var/log/run.log
   ${MG} config:set system/full_page_cache/caching_application LITEMAGE &>> /var/log/run.log

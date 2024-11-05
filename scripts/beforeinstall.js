1abfbd91b140430894195b9e70e297903ccc8ef8
Henadii Sychevskyi <sych74@gmail.com>
2024-11-05 15:59:20 +0100

    Update beforeinstall.js

--- scripts/beforeinstall.js
+++ scripts/beforeinstall.js
@@ -59,7 +59,7 @@ resp.nodes.push({
 }, {
   nodeType: "litespeedphp",
   count: ${settings.cp_count:2},
-  engine: "php8.2",
+  engine: "php8.3",
   flexibleCloudlets: ${settings.cp_flexibleCloudlets:32},
   fixedCloudlets: ${settings.cp_fixedCloudlets:1},
   nodeGroup: "cp",
